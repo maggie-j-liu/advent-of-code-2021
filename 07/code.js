4e38d78af9d15cf140cee64265575f0c5ad2b7c3
maggie-j-liu <maggie.j.liu@gmail.com>
2021-12-06 21:10:20 -0800

    day 7

---
+++ 07/code.js
@@ -0,0 +1,25 @@
+import getInput from "../utils/getInput.js";
+
+const input = await getInput(import.meta.url);
+let positions = input.split(",").map(Number);
+
+const triangle = (n) => (n * (n + 1)) / 2;
+let ans1 = 1e9;
+let ans2 = 1e9;
+const max = positions.reduce((a, b) => Math.max(a, b));
+for (let pos = 0; pos < max; pos++) {
+  let cost1 = 0;
+  let cost2 = 0;
+  for (const x of positions) {
+    cost1 += Math.abs(pos - x);
+    cost2 += triangle(Math.abs(pos - x));
+  }
+  ans1 = Math.min(ans1, cost1);
+  ans2 = Math.min(ans2, cost2);
+}
+
+// part 1
+console.log(ans1);
+
+// part 2
+console.log(ans2);
